5892595a566ce60b2ddef5b457b3359a881c5f01
zhufengnodejs <zhufengnodejs@126.com>
2016-07-20 12:50:29 +0800

    定义一个拷贝文件的任务

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,16 @@
 //然后把gulp模块加载后的返回值赋给gulp变量
 var gulp = require('gulp');
 
-//定义一个任务 task
-/**
- * taskname 任务的名字
- * fn 任务的定义，函数
- */
-gulp.task('copy',function(){
-    /**
-     * 先从源文件中读取源文件
-     * 然后再通过pipe方法以管道的方式导出到目标目录中
-     */
-    gulp.src('./app/index.html')//获取要输入的方法
-        .pipe(gulp.dest('./dist'));//指定要保存的目录
+gulp.task('买米饭',function(){
+    console.log('买米饭');
+});
+gulp.task('买鸡蛋',function(){
+    console.log('买鸡蛋');
+});
+gulp.task('做饭',['买米饭','买鸡蛋'],function(){
+    console.log('炒蛋炒饭');
 });
 
-
+gulp.task('吃饭',['做饭'],function(){
+    console.log('开吃');
+});
