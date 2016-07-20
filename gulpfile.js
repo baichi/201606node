caa10196d7ac175ff10ac3c797c8bb9f2f7154fb
zhufengnodejs <zhufengnodejs@126.com>
2016-07-20 18:04:00 +0800

    压缩并合并CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 var gulp =require('gulp');
 var $ = require('gulp-load-plugins')();
-
-gulp.task('js',function(){
-    gulp.src(['./app/js/b.js','./app/js/a.js'])
-        .pipe($.concat('all.js'))//指定合并后的文件名
-        .pipe($.uglify())//再进行压缩
-        .pipe(gulp.dest('./dist/js'))
+var rename = require('./rename');
+gulp.task('less',function(){
+    //使用此插件
+    gulp.src('./less/less/*.less')
+        .pipe($.less())
+        .pipe($.concat('all.css'))//把多个CSS文件合并成一个CSS文件
+        .pipe(gulp.dest('./dist/css'))
+        .pipe($.minifyCss())
+        .pipe($.rename(rename))
+        .pipe(gulp.dest('./dist/css'))
 });
---
+++ rename.js
@@ -0,0 +1,5 @@
+module.exports = function (path) {
+    //path.dirname += "/ciao";//当前目录
+    path.basename += ".min";//文本基本名 除了后缀之外的名称
+    //path.extname = ".md" //扩展名 文件类型
+}
